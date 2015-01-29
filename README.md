663853295e94e4ad3a9a6aa2acbaa069f11cd306
uccs-tdoan <tdoan@uccs.edu>
2015-01-28 20:14:26 -0800

    add SVM

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # predictRunTime 
+
+
+
+Note: To make libSVM, LinearSVM work:
+
+To use the libSVM library, you only need to use the "Package Manager" of the weka application and install 
+the libSVM package, similar LinearSVM.
+
+Finally from the java project, you have to add the LibSVM library that was created by the "Package Manager"
+to the Classpath.
+
+Usually it's in the "(HOME)\wekafiles\packages\LibSVM" directory.
+
--- performances.java
+++ performances.java
@@ -1,8 +1,9 @@
-/* performance.java is in companion with ClassifiersCost.java (with Misclassifier Cost) 
+/* performance.java is in companion with ClassifiersCost.java (with Misclassifier Cost)
+ * 
  * Tri Doan
  * Note: weka developer 3.7.2
  * Implement different classifiers in order to get statistical summaries   
- * Date: Jan 28
+ * Date: 2015, Jan 28
  * */
 import java.io.BufferedReader;
 import java.io.File;
@@ -106,11 +107,13 @@ public class performances {
 		
 		String inFile;
 		String outStore= outPath +"\\"+outFile;
-	
-		PrintWriter out =new PrintWriter(new FileWriter(outStore));
-		float avgAcc, avgRMSE,avgKappa,avgFscore ,avgPRC,avgAUC ,avgErr,avgCost,newEndTime;
+	 
+		PrintWriter out =new PrintWriter(new FileWriter(outStore,true));
+		
+		float avgAcc, avgRMSE,avgFscore ,avgPRC,avgAUC ,avgErr,avgCost;
 		// For each training-testing split pair, train and test the classifier
-        long startTime ;
+	    long startTrain,startTest, totalTrain=0,totalTest=0 ;
+		
         
      // Use a set of classifiers
      		Classifier[] models = { 
@@ -125,7 +128,7 @@ public class performances {
      				new SMO(),
      				
      				
-                //    new CAAR(),   
+                 //   new CAAR(),   
      				new IBk(), // instance based classifier used K nearest neighbor
      				new KStar(),  // instance based classifier using entropy based distance 
      				new LWL(), // Locally weighted learning used KNN
@@ -164,10 +167,6 @@ public class performances {
      			//	new SimpleCart(),
      				 
      				
-     				
-     		
-     				
-//     				new SMOreg(), //SMOreg implements the support vector machine for regression.				
      		};
 
 		for (int k=0; k< results.size();k++) {
@@ -192,7 +191,7 @@ public class performances {
 		 
         // Print header
 		 
-		// out.println("Accuracy,RMSE,Fscore,Kappa,PRC,AUC,SAR,Dataset,nInstances,nClasses,nAttributes,Algorithm,RunTime,avgCost");
+		out.println("Accuracy,RMSE,Fscore,PRC,AUC,SAR,Dataset,nInstances,nClasses,nAttributes,Algorithm,trainTime,testTime");
 		// Run for each model
 		for (int j = 0; j < models.length; j++) {
  
@@ -200,19 +199,23 @@ public class performances {
 			// Collect every group of predictions for current model in a FastVector
 			FastVector predictions = new FastVector();
           
-            avgAcc=0; avgRMSE=0; avgKappa=0;avgFscore =0 ;avgPRC=0 ;avgAUC=0 ; avgErr=0 ;avgCost=0; newEndTime=0;
+            avgAcc=0; avgRMSE=0; avgFscore =0 ;avgPRC=0 ;avgAUC=0 ; avgErr=0 ;avgCost=0;
+            totalTrain=0; totalTest=0;
 			// For each training-testing split pair, train and test the classifier
-            startTime = System.currentTimeMillis();
-            
+           
+            int count=0;
             System.out.printf(" Number of split %d",trainingSplits.length);
 			for (int i = 0; i < trainingSplits.length; i++) {
+				startTrain = System.currentTimeMillis();
 				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
+				totalTrain = totalTrain + System.currentTimeMillis()- startTrain;
+				startTest  = System.currentTimeMillis() ;
 				predictions.appendElements(validation.predictions());
-				
+				totalTest  = totalTest+ System.currentTimeMillis() - startTest;
 				 avgAcc += validation.pctCorrect();
 					
 	             avgRMSE   += validation.rootMeanSquaredError();
-	             avgKappa  += validation.kappa();
+	           
 	             avgErr    += validation.errorRate(); 
 	             avgCost   += validation.avgCost(); //  total cost of misclassifications (incorrect plus unclassified) over the total number of instances.
 	                           
@@ -222,33 +225,33 @@ public class performances {
 	               
 			} // End for loop - running for one model 
 			
-			final long duration = System.currentTimeMillis() - startTime;
+			
 			
 			System.out.println(" RMSE = "+String.format("%.2f", avgRMSE/trainingSplits.length));
 			System.out.println(" Fscore = "+String.format("%.2f", avgFscore/trainingSplits.length));
-			System.out.println(" kappa = "+String.format("%.2f", avgKappa/trainingSplits.length));
 			System.out.println(" avgPRC = "+String.format("%.2f",(float) avgPRC/trainingSplits.length));
 			System.out.println(" avgAUC = "+String.format("%.2f",(float) avgAUC/trainingSplits.length));
 			
-			System.out.println(" running time = "+String.format("%d",(long) duration));
+			System.out.println(" trainning time = "+String.format("%d",(long) totalTrain));
+			System.out.println(" test time = "+String.format("%d",(long) totalTest));
+			
 			//System.out.println(" ratio of run time over training time = "+String.format("%f",(float)  totalTrainingTime/ duration ));
 			// Calculate overall accuracy of current classifier on all splits
 			//double accuracy = calculateAccuracy(predictions);
 			avgAcc   = avgAcc/trainingSplits.length;
 			avgRMSE   = avgRMSE/trainingSplits.length;
 			avgFscore = avgFscore/trainingSplits.length;
-			avgKappa  = avgKappa/trainingSplits.length;
 			avgPRC    = avgPRC/trainingSplits.length;
 			avgAUC    = avgAUC/trainingSplits.length;
 			double avgSAR = (avgAcc+avgAUC+(1-avgRMSE))/3;	
 			
 			out.print(String.format("%.2f",avgAcc)+","+String.format("%.2f",avgRMSE)+","+String.format("%.2f",avgFscore)+",");
-			out.print(String.format("%.2f",avgKappa)+","+String.format("%.2f",avgPRC)+","+String.format("%.2f",avgAUC)+",");
+			out.print(String.format("%.2f",avgPRC)+","+String.format("%.2f",avgAUC)+",");
 			out.print(String.format("%.2f",avgSAR)+","+ inFile.split("\\.")[0]+","+String.format("%d", data.numInstances())+",");
 			out.print(String.format("%d",data.numClasses())+","+String.format("%d", data.numAttributes()-1)+",");
-		    out.println(models[j].getClass().getSimpleName()+","+String.format("%d",duration)+","+String.format("%.2f",avgCost));
+		    out.print(models[j].getClass().getSimpleName()+","+String.format("%d",totalTrain)+",");
+		    out.println(String.format("%d",totalTest));
 			
-		    
 		    // Print current classifier's name and accuracy in a complicated,
 			
 			System.out.println(j);
